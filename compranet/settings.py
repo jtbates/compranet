3c593a12112eb340c3f7b5490fceca810ded3c06
Jordan T. Bates <jtbates@gmail.com>
2016-11-30 15:20:49 -0600

    Add alembic to config

--- compranet/settings.py
+++ compranet/settings.py
@@ -10,6 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 config = configparser.ConfigParser()
 config_path = os.path.join(BASE_DIR, 'config.ini')
 config.read(config_path)
+main_cfg = config['compranet']
 
 def rel2abs(path, base_dir=BASE_DIR):
     if os.path.isabs(path):
@@ -17,10 +18,12 @@ def rel2abs(path, base_dir=BASE_DIR):
     else:
         return(os.path.join(BASE_DIR, path))
 
-RAW_DIR = rel2abs(config['compranet']['raw_dir'])
-INTERIM_DIR = rel2abs(config['compranet']['interim_dir'])
-SQLITE_DB_PATH = rel2abs(config['compranet']['sqlite_db_path'])
+RAW_DIR = rel2abs(main_cfg['raw_dir'])
+INTERIM_DIR = rel2abs(main_cfg['interim_dir'])
+SQLITE_DB_PATH = rel2abs(main_cfg['sqlite_db_path'])
 DB_URI = "sqlite:///{}".format(SQLITE_DB_PATH)
+ALEMBIC_INI_PATH = rel2abs(main_cfg['alembic_ini_path'])
+ALEMBIC_SCRIPT_LOCATION = rel2abs(main_cfg['alembic_script_location']))
 
 # Set up logging
 fileConfig(config_path)
