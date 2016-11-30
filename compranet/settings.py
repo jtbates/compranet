6de5ccdcf4d46aeda851367e03bb1fc8a3ab7638
Jordan T. Bates <jtbates@gmail.com>
2016-11-30 15:21:34 -0600

    Fix typo

--- compranet/settings.py
+++ compranet/settings.py
@@ -23,7 +23,7 @@ INTERIM_DIR = rel2abs(main_cfg['interim_dir'])
 SQLITE_DB_PATH = rel2abs(main_cfg['sqlite_db_path'])
 DB_URI = "sqlite:///{}".format(SQLITE_DB_PATH)
 ALEMBIC_INI_PATH = rel2abs(main_cfg['alembic_ini_path'])
-ALEMBIC_SCRIPT_LOCATION = rel2abs(main_cfg['alembic_script_location']))
+ALEMBIC_SCRIPT_LOCATION = rel2abs(main_cfg['alembic_script_location'])
 
 # Set up logging
 fileConfig(config_path)
